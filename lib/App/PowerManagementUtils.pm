1bbff2c77dca8a60d409533e99a78c336664462c
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-09-25 10:53:08 +0700

    Add note about the persistence of sleep prevention

--- lib/App/PowerManagementUtils.pm
+++ lib/App/PowerManagementUtils.pm
@@ -1,6 +1,8 @@
 package App::PowerManagementUtils;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
 use 5.010001;
@@ -18,6 +20,10 @@ Uses <pm:Proc::Govern> to run a command, with the option `no-sleep' to instruct
 Proc::Govern to disable system from sleeping while running the command. For more
 options when running command, e.g. timeout, load control, autorestart,
 screensaver control, use the module or its CLI <prog:govproc> directly.
+
+Note that sleep prevention survives reboot, so if this script is terminated
+prematurely before it can unprevent sleep again, you'll need to invoke
+<prog:unprevent-sleep> to restore normal sleep.
 
 _
     args => {
@@ -50,6 +56,10 @@ $SPEC{prevent_sleep_until_interrupted} = {
 Uses <pm:Proc::Govern> to run `sleep infinity`, with the option `no-sleep' to
 instruct Proc::Govern to disable system from sleeping. To stop preventing sleep,
 you can press Ctrl-C.
+
+Note that sleep prevention survives reboot, so if this script is terminated
+prematurely before it can unprevent sleep again, you'll need to invoke
+<prog:unprevent-sleep> to restore normal sleep.
 
 _
     args => {
